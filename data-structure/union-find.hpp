016431489197053b4cef2885bc9d6c3f7f330e75
Dustin Miao <dutin20.meow@gmail.com>
2021-12-29 22:06:41 -0800

    Updated DSU

--- data-structure/union-find.hpp
+++ data-structure/union-find.hpp
@@ -1,77 +1,38 @@
-/**
- * @brief A container that supports the union of disjoint sets and the finding
- * of the roots of forests with Small-to-Large Merging. For a dynamic version
- * see Persistant Union Find or Link-Cut Tree. Useful for Kruskal's MST algorithm.
- */
 struct UnionFind {
-    vector<int> par, sze;
-    int max_set; // the size of the largest set
+    vector<int> parent, setsize;
 
-    /**
-     * @brief Constructs the UnionFind with size zero
-     */
-    explicit UnionFind() {}
+    UnionFind() = default;
 
-    /**
-     * @brief Constructs the UnionFind with a given size
-     * @param n A positive integer that specifies the size
-     */
     UnionFind(int n) { init(n); }
 
-    /**
-     * @brief Initializes the UnionFind with a given size in
-     * linear time.
-     * @param n A positive integer that specifies the size
-     */
     void init(int n) {
-        par.resize(n);
-        sze.resize(n);
-
-        iota(par.begin(), par.end(), 0);
-        fill(sze.begin(), sze.end(), 1);
-        max_set = 1;
+        parent.resize(n);
+        setsize.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        fill(setsize.begin(), setsize.end(), 1);
     }
 
-    /**
-     * @brief Determines the root of the tree where a node is located in
-     * amortized O(alpha(n)), where alpha is the inverse ackermann function.
-     * @param u A non-negative integer that specifies the node
-     */
     int find(int u) {
-        if (u == par[u])
+        if (u == parent[u])
             return u;
-        return par[u] = find(par[u]);
+        return parent[u] = find(parent[u]);
     }
 
-    /**
-     * @brief Merges the trees where two nodes are located in
-     * amortized O(alpha(n)), where alpha is the inverse ackermann function.
-     * Does nothing if the two nodes are already in the same tree.
-     * @param u A non-negative integer that specifies the first node
-     * @param v A non-negative integer that specifies the second node
-     * @return true, if the merge was successfully completed;
-     * false, otherwise
-     */
     bool merge(int u, int v) {
         u = find(u), v = find(v);
         if (u == v)
-            return 0;
-
-        if (sze[u] > sze[v]) {
-            par[v] = u;
-            max_set = max(max_set, sze[u] += sze[v]);
+            return false;
+        if (setsize[u] > setsize[v]) {
+            parent[v] = u;
+            setsize[u] += setsize[v];
+        } else {
+            parent[u] = v;
+            setsize[v] += setsize[u];
         }
-        else {
-            par[u] = v;
-            max_set = max(max_set, sze[v] += sze[u]);
-        }
-        return 1;
+        return true;
     }
 
-    /**
-     * @return Returns the size of the tree containing a given node
-     */
     int size(int u) {
-        return sze[find(u)];
+        return setsize[find(u)];
     }
 };
--- docs/data-structure/union-find.md
+++ docs/data-structure/union-find.md
@@ -3,4 +3,20 @@ title: Union find
 documentation_of: //data-structure/union-find.hpp
 ---
 
-Placeholder
+## Union Find
+
+### Summary
+
+Union Find is a data structure that maintains several disjoint sets. It can support the following operations:
+- Given a vertex $u$, returns the root of the set of that contains $u$.
+- Given two vertices $u, v$, merge them into a single set, provided they are not already in the same set.
+The Union Find data structure supports both these operations in $\mathcal{O}(\alpha(n))$ time, where $n$ is the number of vertices maintained and $\alpha(n)$ is the inverse of the [Ackermann function](https://en.wikipedia.org/wiki/Ackermann_function). Note that $\alpha(n) \leq 6$ for all reasonable $n$ and can be regarded as a constant.
+
+### Documentation
+
+- `UnionFind()`: Constructs an empty `UnionFind` object. Works in $\mathcal{O}(1)$.
+- `UnionFind(int n)`: Constructs a `UnionFind` object with $n$ disjoint sets. Works in $\mathcal{O}(n)$.
+- `init(int n)`: Initializes the object such that it can store $n$ disjoing sets. Works in $\mathcal{O}(n)$.  
+- `find(int u)`: Finds the root of the set in which $u$ is contained. Works in amortized $\mathcal{O}(\alpha(n))$.
+- `merge(int u, int v)`: Merges the sets containing $u$ and $v$ into a single set, provided that they start out in different sets. Returns a boolean value indicating whether this operation was successful. Works in amortized $\mathcal{O}(\alpha(n))$.
+- `size(int u)`: Finds the size of the set that contains $u$. Works in amortized $\mathcal{O}(\alpha(n))$
