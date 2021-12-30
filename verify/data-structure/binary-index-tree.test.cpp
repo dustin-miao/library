6fcb8bba1e8c49a52020625e91fb181357afdd2f
Dustin Miao <dutin20.meow@gmail.com>
2021-12-30 08:06:01 -0800

    Create binary-index-tree.test.cpp

---
+++ verify/data-structure/binary-index-tree.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data-structure/binary-index-tree.hpp"
+
+const int MAX = 5e5 + 5;
+
+int N, Q;
+long long A[MAX];
+FenwickTree<long long> bit;
+
+int main() {
+  cin >> N >> Q;
+  for (int i = 0; i < N; i++)
+    cin >> A[i];
+  bit = FenwickTree(A, A + N);
+  while (Q--) {
+    int t; cin >> t;
+    if (t == 0) {
+      int p; long long x;
+      cin >> p >> x;
+      bit.update(p + 1, x);
+    } else {
+      int l, r;
+      cin >> l >> r;
+      cout << bit.query(l + 1, r) << '\n';
+    }
+  }
+}
