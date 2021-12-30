28c5ddf39470e35fd78857158066c7bdd755c6d9
Dustin Miao <dutin20.meow@gmail.com>
2021-12-30 08:13:25 -0800

    pls work

--- data-structure/binary-index-tree.hpp
+++ data-structure/binary-index-tree.hpp
@@ -1,7 +1,9 @@
-template<typename T> 
+template<typename T>
 struct FenwickTree {
     int n;
     vector<T> tree;
+
+    FenwickTree() = default;
 
     FenwickTree(int _n) : n(_n + 1) {
         tree.assign(n, T());
@@ -21,9 +23,9 @@ struct FenwickTree {
         return ret;
     }
 
-    T query(int l, int r) {
-        return query(r) - query(l - 1);
-    }
+    T query(int l, int r) { return query(r) - query(l - 1); }
+
+    T operator[](int i) { return query(i, i); }
 
     void update(int i, T v) {
         for (; i <= n; i += i & -i)
--- verify/data-structure/binary-index-tree.test.cpp
+++ verify/data-structure/binary-index-tree.test.cpp
@@ -15,7 +15,7 @@ int main() {
   cin >> N >> Q;
   for (int i = 0; i < N; i++)
     cin >> A[i];
-  bit = FenwickTree(A, A + N);
+  bit = FenwickTree<long long>(A, A + N);
   while (Q--) {
     int t; cin >> t;
     if (t == 0) {
