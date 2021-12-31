6de55158fa3a71f01fbfaa567fa71f27f59bf249
Dustin Miao <dutin20.meow@gmail.com>
2021-12-31 10:27:42 -0800

    Update line.hpp

--- geometry/line.hpp
+++ geometry/line.hpp
@@ -4,11 +4,15 @@ struct line {
 
     line() = default;
 
-    line(T _m, T _b) : m(_m), b(_b);
+    line(T _m, T _b) : m(_m), b(_b) {};
 
-    T operator(T x) { return m * x + b; }
+    T operator()(T x) { return m * x + b; }
 
-    friend double intersect(const line &l1, cosnt line &l2) { return (double)(l1.b - l2.b) / (double)(l2.m - l1.m); }
+    friend double intersect(const line &l1, const line &l2) { 
+        return (double)(l1.b - l2.b) / (double)(l2.m - l1.m); 
+    }
 
-    friend ostream &operator<<(ostream &os, const line &l) { return os << "y = " << m << "x + " b; }
+    friend ostream &operator<<(ostream &os, const line &l) { 
+        return os << "y = " << l.m << "x + " << l.b; 
+    }
 };
