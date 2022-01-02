8e23adb6405e04cc886b64c6bf4933794592deba
Dustin Miao <dutin20.meow@gmail.com>
2022-01-02 09:52:56 -0800

    Create recursive-segment-tree.yosupo-point-set-range-composite.test.cpp

---
+++ verify/data-structure/recursive-segment-tree.yosupo-point-set-range-composite.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data-structure/recursive-segment-tree.hpp"
+
+const int MAX = 5e5 + 5;
+
+
+struct line {
+
+};
