ad195e87e551a02d9fa3afce85523d555c1da799
Dustin Miao <dutin20.meow@gmail.com>
2022-01-02 10:03:07 -0800

    Update recursive-segment-tree.yosupo-point-set-range-composite.test.cpp

--- verify/data-structure/recursive-segment-tree.yosupo-point-set-range-composite.test.cpp
+++ verify/data-structure/recursive-segment-tree.yosupo-point-set-range-composite.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
 
 #include <bits/stdc++.h>
 using namespace std;
