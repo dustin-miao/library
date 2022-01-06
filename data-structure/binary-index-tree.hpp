b50143b96cd08e7e7f36d94a7aab9db3fe056612
Dustin Miao <67491820+dutinmeow@users.noreply.github.com>
2022-01-05 19:16:14 -0800

    Update binary-index-tree.hpp

--- data-structure/binary-index-tree.hpp
+++ data-structure/binary-index-tree.hpp
@@ -1,35 +1,35 @@
 template<typename T>
 class fentree {
-    int n;
-    vector<T> tree;
+	int n;
+	vector<T> tree;
 
 public:
-    void init(int _n) {
-        n = _n + 1;
-        tree.assign(n, T());
-    }
+	void init(int _n) {
+		n = _n;
+		tree.assign(n + 1, T());
+	}
 
-    template<class Iterator>
-    void init(Iterator begin, Iterator end) {
-        n = distance(begin, end) + 1;
-        tree.resize(n);
-        for (int i = 1; begin != end; i++, begin++)
-            update(i, *begin);
-    }
+	template<class Iterator>
+	void init(Iterator begin, Iterator end) {
+		n = distance(begin, end);
+		tree.resize(n + 1);
+		for (int i = 1; begin != end; i++, begin++)
+			update(i, *begin);
+	}
 
-    void update(int i, T v) {
-        for (; i <= n; i += i & -i)
-            tree[i] += v;
-    }
+	void update(int i, T v) {
+		for (; i <= n; i += i & -i)
+			tree[i] += v;
+	}
 
-    T query(int i) {
-        T ret = 0;
-        for (; i; i -= i & -i)
-            ret += tree[i];
-        return ret;
-    }
+	T query(int i) {
+		T ret = 0;
+		for (; i; i -= i & -i)
+			ret += tree[i];
+		return ret;
+	}
 
-    T query(int l, int r) { return query(r) - query(l - 1); }
+	T query(int l, int r) { return query(r) - query(l - 1); }
 
-    T operator[](int i) { return query(i, i); }
+	T operator[](int i) { return query(i, i); }
 };
