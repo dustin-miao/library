10a26591e8016d689e09d98b441ae262d5a1becf
Dustin Miao <dutin20.meow@gmail.com>
2022-01-22 21:57:34 -0800

    work pls

---
+++ verify/recursive-segment-tree.yosupo-point-set-range-composite.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_set_range_composite"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data-structure/recursive-segment-tree.hpp"
+
+const int MAX = 5e5 + 5;
+const long long MOD = 998244353;
+
+int N, Q;
+
+struct stinfo {
+	struct node { long long a, b; };
+
+	using T = node;
+
+	const T dval = {1, 0};
+
+	void apply(T &a, T b) { a = b; }
+
+	T merge(T a, T b) { return {a.a * b.a % MOD, (b.a * a.b + b.b) % MOD}; }
+};
+
+Segtree<stinfo> sgt;
+
+int main() {
+	cin >> N >> Q;
+	sgt.init(N);
+	for (int i = 0; i < N; i++) {
+		long long a, b; 
+		cin >> a >> b;
+		sgt.update(i, {a, b});
+	}
+	while (Q--) {
+		int t; cin >> t;
+		if (t == 0) {
+			int p; long long c, d;
+			cin >> p >> c >> d;
+			sgt.update(p, {c, d});
+		} else {
+			int l, r; long long x;
+			cin >> l >> r >> x;
+			auto [a, b] = sgt.query(l, r - 1);
+			cout << (a * x + b) % MOD << '\n';
+		}
+	}
+}
