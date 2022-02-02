756ac54240268940e6d5cda04cdab168098c2c36
Dustin Miao <dutin20.meow@gmail.com>
2022-02-02 11:52:42 -0800

    Update kruskal.hpp

--- graph/kruskal.hpp
+++ graph/kruskal.hpp
@@ -2,13 +2,17 @@
 #include "graph/graph-util.hpp"
 
 template<typename T>
-graph<T> kruskal(size_t n, const edgelist<T> &E) {
+graph<T> kruskal(size_t n, edgelist<T> E) {
 	graph<T> mst(n);
 	if (n <= 1)
 		return mst;
 	UnionFind dsu(n);
-	sort(edges.begin(), edges.end(), [](const tuple<int, int, T> &a, const tuple<int, int, T> &b) { return get<2>(a) < get<2>(b); });
-	for (auto [u, v, w] : edges) {
+	sort(E.begin(), E.end(), 
+		[](auto a, auto b) { 
+			return get<2>(a) < get<2>(b); 
+		}
+	);
+	for (auto [u, v, w] : E) {
 		if (dsu.merge(u, v)) {
 			mst[u].emplace_back(v, w);
 			mst[v].emplace_back(u, w);
