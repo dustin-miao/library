aaf507fcd73492e01d90033f1530e5b41b78f8b2
Dustin Miao <dutin20.meow@gmail.com>
2022-02-05 09:55:16 -0800

    Added Sparse Table

---
+++ data-structure/sparse-table.hpp
@@ -0,0 +1,26 @@
+template<typename T>
+class SparseTable {
+	size_t n, logn;
+	vector<vector<T>> table;
+
+public:
+	SparseTable() = default;
+
+	template<typename I>
+	SparseTable(I l, I r) { init(l, r); }
+
+	template<typename I>
+	void init(I l, I r) {
+		n = distance(l, r), logn = 32 - __builtin_clz(n);
+		table.assign(logn, vector<T>(n));
+		copy(l, r, table[0].begin());
+		for (int k = 1; k < logn; k++)
+			for (int i = 0; i + (1 << k) <= n; i++)
+				table[k][i] = min(table[k - 1][i], table[k - 1][i + (1 << (k - 1))]);
+	}
+
+	T query(int l, int r) {
+		int k = 31 - __builtin_clz(r - l + 1);
+		return min(table[k][l], table[k][r - (1 << k) + 1]);
+	}
+};
---
+++ verify/sparse-table.yosupo-static-rmq.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data-structure/sparse-table.hpp"
+
+int main() {
+	int N, Q;
+	cin >> N >> Q;
+	vector<int> A(N);
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+	SparseTable<int> st(A.begin(), A.end());
+	while (Q--) {
+		int l, r;
+		cin >> l >> r;
+		cout << st.query(l, r - 1) << '\n';
+	}
+}
