28e5ff7a5096703be3d56790cb1f40f5fb31eefe
Dustin Miao <dutin20.meow@gmail.com>
2022-04-01 11:39:27 -0700

    Update LineContainer

--- data-structure/line-container.hpp
+++ data-structure/line-container.hpp
@@ -1,41 +1,41 @@
 #include "utility/floor-div.hpp"
 
-bool __LINE_COMPARE__;
-
 struct Line {
-	mutable long long m, b, p;
-	bool operator<(const Line &l) const { return __LINE_COMPARE__ ? p < l.p : m < l.m; }
+    mutable long long m, b, p;
+    bool operator<(const Line &o) const { return m < o.m; }
+    bool operator<(long long x) const { return p < x; }
 };
 
-class LineContainer : multiset<Line> {
-	bool intersect(iterator x, iterator y) {
-		if (y == end()) { 
-			x->p = LLONG_MAX; 
-			return false; 
+class LineContainer : multiset<Line, less<>> {
+    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    static const long long inf = INT_MAX;
+
+    bool isect(iterator x, iterator y) {
+        if (y == end()) {
+			x->p = inf;
+			return false;
 		}
-		if (x->m == y->m) 
-			x->p = x->b > y->b ? LLONG_MAX : LLONG_MIN;
-		else 
-			x->p = floor_div(y->b - x->b, x->m - y->m);
-		return x->p >= y->p;
-	}
+        if (x->m == y->m)
+            x->p = x->b > y->b ? inf : -inf;
+        else
+            x->p = floor_div(y->b - x->b, x->m - y->m);
+        return x->p >= y->p;
+    }
 
 public:
-	void add(long long m, long long b) {
-		auto z = insert({m, b, 0}), y = z++, x = y;
-		while (intersect(y, z)) 
-			z = erase(z);
-		if (x != begin() && intersect(--x, y)) 
-			intersect(x, y = erase(y));
-		while ((y = x) != begin() && (--x)->p >= y->p)
-			intersect(x, erase(y));
-	}
-	
-	long long query(long long x) {
-		assert(!empty());
-		__LINE_COMPARE__ = true; 
-		auto l = *lower_bound({0, 0, x}); 
-		__LINE_COMPARE__ = false;
-		return l.m * x + l.b;
-	}
+    void add(long long m, long long b) {
+        auto z = insert({m, b, 0}), y = z++, x = y;
+        while (isect(y, z)) 
+            z = erase(z);
+        if (x != begin() && isect(--x, y)) 
+            isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p) 
+            isect(x, erase(y));
+    }
+
+    long long query(long long x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.m * x + l.b;
+    }
 };
---
+++ geometry/convex-hull.hpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "geometry/point.hpp"
+
+namespace geo {
+	template<typename T>
+	vector<point<T>> convex_hull(vector<point<T>> poly) {
+		if (poly.size() < 2)
+			return poly;
+		int n = poly.size();
+		vector<point<T>> hull;
+		sort(poly.begin(), poly.end());
+		hull.push_back(poly[0]);
+		for (int i = 1; i < n; i++) {
+			auto p = poly[i];
+			while (ret.size() >= 2 && (ret[ret.size() - 1] - ret[ret.size() - 2]) ^ (poly[i] - ret[ret.ize() - 2]) < 0)
+		}
+	}
+}
+
+int main() {
+
+}
