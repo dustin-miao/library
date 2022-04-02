ca03415f52f855fe0fdb12f338cbf3bb9dd2f9d7
Dustin Miao <dutin20.meow@gmail.com>
2022-04-02 13:50:59 -0700

    Create mint.power.test.cpp

---
+++ verify/mint.power.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "utility/mint.hpp"
+
+int main() {
+	MOD = 1e9 + 7;
+
+	mint N; int M;
+	cin >> N >> M;
+	cout << fast_pow(N, M);
+}
