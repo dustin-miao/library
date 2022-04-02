1d0ce4c9c10330a1f712a62bf8458d8d24892401
Dustin Miao <dutin20.meow@gmail.com>
2022-04-02 16:08:08 -0700

    Added Binomial Coefficients

---
+++ combo/binom-coef.hpp
@@ -0,0 +1,22 @@
+#include "utility/mint.hpp"
+
+struct binom_coef {
+	vector<mint> fact, inv_fact;
+
+	binom_coef() = default;
+
+	binom_coef(int n) { init(n); }
+
+	void init(int n) {
+		fact.resize(n + 1);
+		fact[0] = 1;
+		inv_fact.resize(n + 1);
+		inv_fact[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			fact[i] = fact[i - 1] * i;
+			inv_fact[i] = 1 / fact[i];
+		}
+	}
+
+	mint operator()(int n, int k) { return fact[n] * inv_fact[k] * inv_fact[n - k]; }
+};
