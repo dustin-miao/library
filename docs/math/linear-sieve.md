f1637764aa32834b876d974ea457d0824ea00c27
Dustin Miao <dutin20.meow@gmail.com>
2022-04-08 11:38:48 -0700

    Added linear sieve

---
+++ docs/math/linear-sieve.md
@@ -0,0 +1,8 @@
+---
+title: Linear Sieve
+documentation_of: //math/linear-sieve.hpp
+---
+
+## Linear Sieve
+
+I have no idea if this works or not. 
---
+++ math/linear-sieve.hpp
@@ -0,0 +1,12 @@
+pair<vector<int>, vector<int>> linear_sieve(int n) {
+	vector<int> leastFactor(n + 1, 0), primes;
+	for (int i = 2; i <= n; i++) {
+		if (leastFactor[i] == 0) {
+			leastFactor[i] = i;
+			primes.push_back(i);
+		}
+		for (int j = 0; j < primes.size() && primes[j] <= leastFactor[i] && i * primes[j] <= N; j++)
+			leastFactor[i * primes[j]] = primes[j];
+	}
+	return make_pair(leastFactor, primes);
+}
