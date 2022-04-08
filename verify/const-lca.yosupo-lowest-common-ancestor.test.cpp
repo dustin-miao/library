30894d3789b998044a4965bd972d818955a9fdd7
Dustin Miao <dutin20.meow@gmail.com>
2022-04-08 11:24:46 -0700

    Added verification for const lca

---
+++ verify/const-lca.yosupo-lowest-common-ancestor.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/lca"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "data-structure/const-lca.hpp"
+
+int main() {
+	int N, Q;
+	cin >> N >> Q;
+	vector<vector<int>> T(N);
+	for (int i = 1; i < N; i++) {
+		int p;
+		cin >> p;
+		T[p].push_back(i);
+	}
+	LowestCommonAncestor lca(T);
+	while (Q--) {
+		int u, v;
+		cin >> u >> v;
+		cout << lca(u, v) << '\n';
+	}
+}
