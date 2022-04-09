f2f76f369521b58154ea7c598554c7e3345398d2
Dustin Miao <dutin20.meow@gmail.com>
2022-04-08 17:03:50 -0700

    .

---
+++ docs/math/discrete-log.md
@@ -0,0 +1,6 @@
+---
+title: Discrete Logarithm
+documentation_of: //math/discrete-log.hpp
+---
+
+## Discrete Logarithm
--- math/discrete-log.hpp
+++ math/discrete-log.hpp
@@ -1,16 +1,22 @@
 #include "utility/mint.hpp"
 
-int discrete_log(mint a, mint b) {
-    int n = sqrt(MOD) + 1;
+namespace math {
+	int discrete_log(mint a, mint b) {
+		int n = sqrt(MOD) + 1;
 
-    mint an = fast_pow(a, n);
-	map<mint, int> vals;
-    for (int q = 0, cur = b; q <= n; q++, cur *= a) 
-        vals[cur] = q;
-    for (int p = 1, cur = 1; p <= n; p++) {
-        cur *= an;
-        if (vals.find(cur) != vals.end()) 
-            return int(n * p - vals[cur]);
-    }
-    return -1;
+		mint an = fast_pow(a, n);
+		map<mint, int> vals;
+		mint cur = b;
+		for (int q = 0; q <= n; q++) { 
+			vals[cur] = q;
+			cur *= a;
+		}
+		cur = 1;
+		for (int p = 1; p <= n; p++) {
+			cur *= int(an);
+			if (vals.find(cur) != vals.end()) 
+				return int(n * p - vals[cur]);
+		}
+		return -1;
+	}
 }
