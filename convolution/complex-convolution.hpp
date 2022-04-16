c37c41dfc7c697b7518ed204b5e62fd62fb201ab
Dustin Miao <dutin20.meow@gmail.com>
2022-04-16 09:52:29 -0700

    Added complex convolution

---
+++ convolution/complex-convolution.hpp
@@ -0,0 +1,29 @@
+#include "convolution/fast-fourier-transform.hpp"
+
+namespace conv {
+	template<typename T, typename U = double>
+	vector<T> complex_convolution(const vector<complex<T>> &a, const vector<complex<T>> &b) {
+		int n = 1;
+		while (n < a.size() + b.size()) 
+			n <<= 1;
+		vector<complex<U>> pa(n), pb(n);
+		for (int i = 0; i < a.size(); i++)
+			pa[i] = complex<U>(a[i].real(), a[i].imag());
+		for (int i = 0; i < b.size(); i++)
+			pb[i] = complex<U>(b[i].real(), b[i].imag());
+
+		fast_fourier_transform(pa);
+		fast_fourier_transform(pb);
+		vector<complex<U>> c(n);
+		for (int i = 0; i < n; i++) 
+			c[i] = pa[i] * pb[i] / static_cast<U>(n);
+		reverse(c.begin() + 1, c.end());
+		fast_fourier_transform(c);
+
+		n = a.size() + b.size() - 1;
+		vector<T> ret(n);
+		for (int i = 0; i < n; i++)
+			ret[i] = static_cast<T>(c[i].real() + 0.5);
+		return ret;
+	}
+}
---
+++ docs/convolution/complex-convolution.md
@@ -0,0 +1,6 @@
+---
+title: Complex Convolution
+documentation_of: //convolution/complex-convolution.hpp
+---
+
+## Complex Convolution
