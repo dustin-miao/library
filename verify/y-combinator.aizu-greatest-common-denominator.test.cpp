1ce6805a3ee06bec40aca42a93cfb9da2686475e
Dustin Miao <dutin20.meow@gmail.com>
2022-04-19 13:01:20 -0700

    Added y combinator

---
+++ verify/y-combinator.aizu-greatest-common-denominator.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "utility/y-combinator.hpp"
+
+int main() {
+	long long A, B;
+	cin >> A >> B;
+
+    auto gcd_test = std::y_combinator([](auto gcd_test, long long a, long long b) -> long long { 
+		return b == 0 ? a : gcd_test(b, a % b); 
+	});
+	
+    cout << gcd_test(A, B) << endl;
+}
