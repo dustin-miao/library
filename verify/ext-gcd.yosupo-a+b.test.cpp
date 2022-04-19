079924eefd09caf31338ba4e8a284aa5b2abf185
Dustin Miao <dutin20.meow@gmail.com>
2022-04-19 12:50:46 -0700

    Added extended gcd

---
+++ verify/ext-gcd.yosupo-a+b.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "math/ext-gcd.hpp"
+#include "random/mersenne_twister.md"
+
+int main() {
+	int T = 2e5;
+	while (T--) {
+		long long A = rng<long long>(1, 1e9);
+		long long B = rng<long long>(1, 1e9);
+		long long X, Y;
+		auto G = math::ext_gcd(A, B, X, Y);
+		assert(G == __gcd(A, B));
+		assert(A * X + B * Y == G);
+	}
+
+	long long A, B;
+	cin >> A >> B;
+	cout << A + B << '\n';
+}
