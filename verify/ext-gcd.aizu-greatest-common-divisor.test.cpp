a93d177b630f54a5d821c146f86c55211efdaaab
Dustin Miao <dutin20.meow@gmail.com>
2022-04-19 13:04:18 -0700

    Added y combinator

--- verify/ext-gcd.aizu-greatest-common-divisor.test.cpp
+++ verify/ext-gcd.aizu-greatest-common-divisor.test.cpp
@@ -1,1 +1,12 @@
-#include 
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_B"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "math/ext-gcd.hpp"
+
+int main() {
+	long long A, B, X, Y;
+	cin >> A >> B;
+	cout << math::ext_gcd(A, B, X, Y) << '\n';
+}
